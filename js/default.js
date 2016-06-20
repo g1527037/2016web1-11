84b76555effda38ea4da42b8b39011885dab9551
g1527037 <g1527037@st.t-kougei.ac.jp>
2016-06-20 17:37:01 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,1 +1,16 @@
 
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム　０～３の数字を入力してください'));
+
+var message;
+if(answer === number) {
+  message = 'あたり';
+}else if(answer < number){
+  message = 'もっと大きい';
+}else if(number < answer){
+  message = 'もっと小さい';
+}else{
+  message = '0~3を入力';
+}
+
+document.getElementById('choice').textContent = message;
